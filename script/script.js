7aa272cd9fe183cb6535d20e3eb3a72975bb6edf
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-03-05 18:17:04 -0300

    Add button javascript logic -Used eventlistener with a callback function to use prompt messages to request number between 1 and 100 from the user filtering negative or string

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector("#container");
+const clearBtn = document.querySelector("#clear-btn");
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
 }
+
+clearBtn.addEventListener("click", () => {
+    const size = Number(prompt("Enter number of squares per side:"));
+
+    if(!size || size > 100 || size < 1) {
+        alert("Please enter a number between 1 and 100.");
+        return;
+    }
+    console.log(size);
+});
 
