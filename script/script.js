fabddb4fc37d8f8754190f63e1f5e73d4333589f
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-03-05 18:42:32 -0300

    Add clearbtn logic and update for loop -The button clears all the divs and them prints them again according to the user input on the prompt method. -The for loop update is using the user imput as an argument to create new grids, using 16x16 as a placeholder like grid size.

--- script/script.js
+++ script/script.js
@@ -1,11 +1,24 @@
 const container = document.querySelector("#container");
 const clearBtn = document.querySelector("#clear-btn");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+function createGrid(size) {
+    
+    container.innerHTML = "";
+
+    const containerSize = 480;
+    const squareSize = containerSize / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        container.appendChild(square);
+    }
 }
+createGrid(16);
 
 clearBtn.addEventListener("click", () => {
     const size = Number(prompt("Enter number of squares per side:"));
@@ -14,6 +27,6 @@ clearBtn.addEventListener("click", () => {
         alert("Please enter a number between 1 and 100.");
         return;
     }
-    console.log(size);
+    createGrid(size);
 });
 
